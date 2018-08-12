e105bb448730042b92baf24179aab73b17311171
vlavik007 <vlavik007@gmail.com>
2018-08-12 20:52:30 +0300

    create new service related with OAuth2 authentication

---
+++ auth-service/src/main/java/com/vlavik/bct/auth/AuthServiceApplication.java
@@ -0,0 +1,16 @@
+package com.vlavik.bct.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@SpringBootApplication
+@EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class AuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServiceApplication.class, args);
+    }
+}
